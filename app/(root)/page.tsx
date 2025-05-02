d9a501f000cbd3dd89f8698f8d0e7420961586c4
Aashish Panthee <aashishpanthee060@gmail.com>
2025-05-02 20:47:16 +0545

    (chore) app creation & basic layout - add assets - shadcn UI setup - setup root layout & constants - add header & footer components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <div className=''>Wardrobe</div>;
+};
+
+export default Homepage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Modern Wardrobe";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Your modern clothing store";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
